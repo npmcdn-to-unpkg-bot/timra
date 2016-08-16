294f854f52525ddf7a714f79e94c387a4d0fac80
Anders Roos <anders.roos@gmail.com>
2016-08-16 18:20:09 +0200

    Made data visible on site.

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ var morgan = require('morgan')
 var app = express()
 var templating = require('jade')
 
+//var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/timra');
+var attractions = db.get('attractions');
+
 var index = templating.compileFile(__dirname + '/src/template/index.jade')
-//var attraction = templating.compileFile(__dirname + '/src/template/attraction.jade')
+var attraction = templating.compileFile(__dirname + '/src/template/attraction.jade')
+
 
 app.use(morgan('dev'))
 app.use(express.static(__dirname + '/static'))
 
+
 app.get('/', function(req, res) {
-    res.send(index({title: 'Home'}))
+    attractions.find().then((docs) => {
+        res.send(index({
+            title: 'Home',
+            attractions: docs
+        }))
+    });
 })
 
 app.get('/attraction/*-:id', function(req, res) {
     res.send(index({title: req.params.id}))
 })
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('listening on http://localhost:' + (process.env.PORT || 3000))
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('listening on http://localhost:' + port)
 })
